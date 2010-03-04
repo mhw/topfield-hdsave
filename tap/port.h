895b43de4356821a70fc6e843b3ddbdf515217dc
Mark H. Wilkinson <mhw@kremvax.net>
2010-03-04 14:36:07 +0000

    Add enough to build some .o files using the Topfield compiler.

---
+++ tap/port.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <sys/types.h>
+
+typedef unsigned char uint8_t;
+typedef __uint16_t uint16_t;
+typedef __uint32_t uint32_t;
+typedef __uint64_t uint64_t;
+
+#define PRId32		"d"
+
+#define PRIx16		"x"
+#define PRIx32		"x"
